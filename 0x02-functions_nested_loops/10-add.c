6148ef3cdb34255bc34b8cfc8e1dd680eab9d709
DeborahSarpong <dasarpong008@st.ug.edu.gh>
2023-02-22 19:44:32 -0800

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -17,20 +17,3 @@
 
 		return (c);
 	}
-
-#include "main.h"
-#include <stdio.h>
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    int n;
-
-    n = add(89, 9);
-    printf("%d\n", n);
-    return (0);
-}
