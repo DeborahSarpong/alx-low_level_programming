2b8805d4df4c3a36c63e16acb7eae14bcc18d03d
DeborahSarpong <dasarpong008@st.ug.edu.gh>
2023-02-22 21:42:36 -0800

    102-fibonacci.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,9 +3,7 @@
 * Auth: vincent kip
 */
 
-
 #include <stdio.h>
-
 
 /**
 * main - Lists all the natural numbers below 1024 (excluded)
@@ -15,14 +13,14 @@
 */
 int main(void)
 {
-		int i, sum = 0;
+int i, sum = 0;
 
 
-		for (i = 0; i < 1024; i++)
-		{
-			if ((i % 3) == 0 || (i % 5) == 0)
-				sum += i;
-		}
+for (i = 0; i < 1024; i++)
+{
+if ((i % 3) == 0 || (i % 5) == 0)
+sum += i;
+}
 
 
 		printf("%d\n", sum);
