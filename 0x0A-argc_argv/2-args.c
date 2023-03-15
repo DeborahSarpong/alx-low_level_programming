3c6d2d095c64d85aaf272304e72447b7ddad354b
DeborahSarpong <dasarpong008@st.ug.edu.gh>
2023-03-15 16:46:47 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,18 +1,30 @@
-#include "main.h"
+s (22 sloc)  332 Bytes
+
 #include <stdio.h>
+#include "main.h"
 
 /**
- * main - program that prints the number of arguments passed into it
- * @argc: number of arguments
- * @argv: the vector that hold the arguments
- * Return: 0, it's void
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return: Always 0 (Success)
  */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	(void) argv;
 
-	printf("%d\n", argc - 1);
+	int count = 0;
 
+	if (argc > 0)
+	{
+
+		while (count < argc)
+		{
+
+			printf("%s\n", argv[count]);
+			count++;
+		}
+	}
 	return (0);
 }
